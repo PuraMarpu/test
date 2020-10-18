752ebc948010e218ad96650009670ba82608f036
Blank <takerpuri@gmail.com>
2020-10-18 19:59:13 +0530

    plat added

--- sketch.js
+++ sketch.js
@@ -17,15 +17,18 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+
+    plat = new Ground(90,330,200,200);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
+
+    pig1 = new Pig(810, 350);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
@@ -40,25 +43,29 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+
     Engine.update(engine);
+
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+
     box1.display();
     box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
     box5.display();
+
+    ground.display();
+    plat.display();
+
+    log1.display();
+    log3.display();
     log4.display();
     log5.display();
 
+    pig1.display();
+    pig3.display();
+
     bird.display();
-    platform.display();
 }
